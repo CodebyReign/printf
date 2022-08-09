794eee507a5cc2fee8d4a191b81f32a38279c39f
Trizahmuhoi <muhoiteresiah@gmail.com>
2022-08-09 12:53:28 -0700

    Printf.c

--- _printf.c
+++ _printf.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include "main.h"
-=======
-<<<<<<< HEAD
-#include "main.h"
-=======
-#include "holberton.h"
->>>>>>> eda4dac69d56e625ad275b70af077a55c7cf33b5
 
->>>>>>> 8aa83360eb3e34cfea81b91aeb6a2529a41b4ea3
 /**
  * _printf - function my printf
  * @format: string whit format to print
@@ -23,13 +15,11 @@ int length = 0;
 
 if (format == NULL)
 return (-1);
-<<<<<<< HEAD
-va_start(args, format);
-=======
 
 va_start(args, format);
 
->>>>>>> 8aa83360eb3e34cfea81b91aeb6a2529a41b4ea3
+va_start(args, format);
+
 length = _print_format(format, args);
 va_end(args);
 return (length);
--- main.h
+++ main.h
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-=======
 #ifndef MAIN_H
 #define MAIN_H
->>>>>>> ca84b663de4619c6a22ef58334e74f39494a4420
+
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> ca84b663de4619c6a22ef58334e74f39494a4420
->>>>>>> 4cce60951393a88f639bce10481ed4a00c4056ed
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
  * @f: type pointer to function for the conversion specifier
-<<<<<<< HEAD
- *
  */
 
-=======
- */
->>>>>>> ca84b663de4619c6a22ef58334e74f39494a4420
 typedef struct format
 {
 char *id;
@@ -59,12 +45,4 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> ca84b663de4619c6a22ef58334e74f39494a4420
->>>>>>> 4cce60951393a88f639bce10481ed4a00c4056ed
 #endif
